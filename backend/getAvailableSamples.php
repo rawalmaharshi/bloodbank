3f5d6ba9a670c7fc984f9e4dad3b4481ef9a9617
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-03 22:59:54 +0530

    7

--- backend/getAvailableSamples.php
+++ backend/getAvailableSamples.php
@@ -2,11 +2,7 @@
 error_reporting(E_ALL);
 include_once('functions.php');
 
-// $db = getConnection();
-$url = parse_url(getenv("DATABASE_URL"));
-	$host = $url["host"];
-	$db_name = substr($url["path"], 1);
-$db = new mysqli($host,'bb4bd4eceed032', '7728c3e8', $db_name);
+$db = getConnection();
 $fetch = qExecuteAssocArray("SELECT auto_id, blood_group, count, hosp_id, hosp_name, hosp_address, hosp_state, hosp_email FROM blood_samples INNER JOIN hospitals ON blood_samples.hosp_id = hospitals.id");
 if(!$fetch){
 	echo json_encode(array(
