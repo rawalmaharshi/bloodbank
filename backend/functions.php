573b41ec62362c65ea342fa02b35dda14e822da3
Maharshi Rawal <rawalmaharshi@gmail.com>
2018-06-03 23:35:48 +0530

    12

--- backend/functions.php
+++ backend/functions.php
@@ -21,7 +21,6 @@ function &getConnection()
 	try {
 	    $db = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
 	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    echo "Connected successfully"; 
     } catch(PDOException $e)
     {
     	echo "Connection failed: " . $e->getMessage();
